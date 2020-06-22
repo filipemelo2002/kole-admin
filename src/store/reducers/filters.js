42d9d1bc97dbd6beabe2a67d1925fa5e7fbc566e
Filipe Melo <filipemelo032@gmail.com>
2020-06-22 13:55:21 -0300

    Renamed file

---
+++ src/store/reducers/filters.js
@@ -0,0 +1,15 @@
+export default function filters(state = [], action) {
+  if (action.type === 'FILTER_ADD') {
+    return [...state, filters];
+  }
+  if (action.type === 'FILTER_REMOVE') {
+    const filteredFilters = state.filter(
+      (item) => item.id !== action.filter.id,
+    );
+    return filteredFilters;
+  }
+  if (action.type === 'FILTER_FETCH') {
+    return action.filters;
+  }
+  return state;
+}

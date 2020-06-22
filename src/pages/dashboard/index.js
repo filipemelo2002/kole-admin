6d1b55052bc2199aefb8804b70d67b28d3934b9a
Filipe Melo <filipemelo032@gmail.com>
2020-06-22 13:54:42 -0300

    Redux Provider set

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -6,18 +6,13 @@ import {
   BrowserRouter as Router,
 } from 'react-router-dom';
 
-// import { Container } from './styles';
-import {
-  MdQueue,
-  MdMoveToInbox,
-  MdVpnKey,
-  MdPowerSettingsNew,
-} from 'react-icons/md';
 import './style.css';
-import { Sidebar, Menu, Icon } from 'semantic-ui-react';
+import { Menu, Icon } from 'semantic-ui-react';
+import { Provider } from 'react-redux';
 import ManageProducts from '../manage';
 import CustomerOrders from '../customers';
 import ManageAccount from '../account';
+import store from '../../store';
 
 function Dashboard({ history }) {
   const [selected, setSelected] = useState('dashboard');
@@ -35,54 +30,56 @@ function Dashboard({ history }) {
   }, []);
   return (
     <Router>
-      <Menu pointing secondary>
-        <Menu.Item
-          name="Dashboard"
-          as={Link}
-          to="/dashboard"
-          onClick={() => setSelected('dashboard')}
-          active={selected === 'dashboard'}
-        />
-        <Menu.Item
-          name="Orders"
-          as={Link}
-          to="/dashboard/orders"
-          onClick={() => setSelected('orders')}
-          active={selected === 'orders'}
-        />
-        <Menu.Item
-          name="Credentials"
-          as={Link}
-          to="/dashboard/accounts"
-          onClick={() => setSelected('credentials')}
-          active={selected === 'credentials'}
-        />
-        <Menu.Menu position="right">
-          <Menu.Item name="logout" onClick={() => signOut()}>
-            <Icon name="power" />
-            Logout
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
-      <div className="dashboard-container">
-        <div className="container">
-          <Switch>
-            <Route
-              exact
-              path="/dashboard/"
-              component={ManageProducts}
-            />
-            <Route
-              path="/dashboard/orders"
-              component={CustomerOrders}
-            />
-            <Route
-              path="/dashboard/accounts"
-              component={ManageAccount}
-            />
-          </Switch>
+      <Provider store={store}>
+        <Menu pointing secondary>
+          <Menu.Item
+            name="Dashboard"
+            as={Link}
+            to="/dashboard"
+            onClick={() => setSelected('dashboard')}
+            active={selected === 'dashboard'}
+          />
+          <Menu.Item
+            name="Orders"
+            as={Link}
+            to="/dashboard/orders"
+            onClick={() => setSelected('orders')}
+            active={selected === 'orders'}
+          />
+          <Menu.Item
+            name="Credentials"
+            as={Link}
+            to="/dashboard/accounts"
+            onClick={() => setSelected('credentials')}
+            active={selected === 'credentials'}
+          />
+          <Menu.Menu position="right">
+            <Menu.Item name="logout" onClick={() => signOut()}>
+              <Icon name="power" />
+              Logout
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+        <div className="dashboard-container">
+          <div className="container">
+            <Switch>
+              <Route
+                exact
+                path="/dashboard/"
+                component={ManageProducts}
+              />
+              <Route
+                path="/dashboard/orders"
+                component={CustomerOrders}
+              />
+              <Route
+                path="/dashboard/accounts"
+                component={ManageAccount}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Provider>
     </Router>
   );
 }

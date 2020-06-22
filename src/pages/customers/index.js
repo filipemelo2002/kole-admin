c5aafe61cd6e6775824fd5bff0f4252f2ac7b17b
Filipe Melo <filipemelo032@gmail.com>
2020-06-22 11:25:52 -0300

    Orders static table

--- src/pages/customers/index.js
+++ src/pages/customers/index.js
@@ -1,50 +1,77 @@
 import React from 'react';
 
 // import { Container } from './styles';
-
+import { Table } from 'semantic-ui-react';
 import './style.css';
 
 function CustomerOrders() {
   return (
     <div className="customers-orders">
-      <table className="order-list">
-        <tr>
-          <th>Order ID</th>
-          <th>Customer&apos;s Email</th>
-          <th>Status</th>
-          <th>Subtotal</th>
-        </tr>
-        <tr>
-          <td>1223</td>
-          <td>filipemelo032@gmail.com</td>
-          <td>
-            <select placeholder="Section">
-              <option value="">Section</option>
-            </select>
-          </td>
-          <td>CDN$ 250</td>
-        </tr>
-        <tr>
-          <td>1223</td>
-          <td>filipemelo032@gmail.com</td>
-          <td>
-            <select placeholder="Section">
-              <option value="">Section</option>
-            </select>
-          </td>
-          <td>CDN$ 250</td>
-        </tr>
-        <tr>
-          <td>1223</td>
-          <td>filipemelo032@gmail.com</td>
-          <td>
-            <select placeholder="Section">
-              <option value="">Section</option>
-            </select>
-          </td>
-          <td>CDN$ 250</td>
-        </tr>
-      </table>
+      <div className="table">
+        <Table singleLine selectable color="grey">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Registration Date</Table.HeaderCell>
+              <Table.HeaderCell>E-mail address</Table.HeaderCell>
+              <Table.HeaderCell>Order ID</Table.HeaderCell>
+              <Table.HeaderCell>Order Status</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>John Lilki</Table.Cell>
+              <Table.Cell>September 14, 2013</Table.Cell>
+              <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+              <Table.Cell>4452125</Table.Cell>
+              <Table.Cell warning>Pending</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Jamie Harington</Table.Cell>
+              <Table.Cell>January 11, 2014</Table.Cell>
+              <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+              <Table.Cell>54885</Table.Cell>
+              <Table.Cell warning>Pending</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Jill Lewis</Table.Cell>
+              <Table.Cell>May 11, 2014</Table.Cell>
+              <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
+              <Table.Cell>44522</Table.Cell>
+              <Table.Cell positive>Delivered</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Jill Lewis</Table.Cell>
+              <Table.Cell>May 11, 2014</Table.Cell>
+              <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
+              <Table.Cell>44522</Table.Cell>
+              <Table.Cell error>Problem</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Jill Lewis</Table.Cell>
+              <Table.Cell>May 11, 2014</Table.Cell>
+              <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
+              <Table.Cell>44522</Table.Cell>
+              <Table.Cell positive>Delivered</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Jill Lewis</Table.Cell>
+              <Table.Cell>May 11, 2014</Table.Cell>
+              <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
+              <Table.Cell>44522</Table.Cell>
+              <Table.Cell positive>Delivered</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Jill Lewis</Table.Cell>
+              <Table.Cell>May 11, 2014</Table.Cell>
+              <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
+              <Table.Cell>44522</Table.Cell>
+              <Table.Cell positive>Delivered</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </div>
     </div>
   );
 }

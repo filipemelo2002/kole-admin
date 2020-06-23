e9fc92a63f1a1a284fc722cd28b61b2f4c4101f5
Filipe Melo <filipemelo032@gmail.com>
2020-06-23 13:27:13 -0300

    importing correctly

--- src/store/actions/filter.js
+++ src/store/actions/filter.js
@@ -1,4 +1,4 @@
-import { api } from '../../services/api';
+import api from '../../services/api';
 import { getHeaderPayload, updateSessionToken } from '../../utils/handleSessionTokens';
 
 export function addFilter(filter) {
